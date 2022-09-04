4b26e9e0a2a48b9e49c1cc78f01f48b2182b884d
Senali <senalidilumika@gmail.com>
2022-09-04 23:32:42 +0530

    Password field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
+import { FiRefreshCcw } from 'react-icons/fi'
 
-import Navbar from '../components/Navbar'
 import { generatePassword } from '../utils/util'
 
 const Home: NextPage = () => {
@@ -18,6 +18,15 @@ const Home: NextPage = () => {
     setPassword(pass)
   }, [])
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const reloadNewPassword = () => {
+    const pass = generatePassword(size, haveUppercase, haveLowercase, haveNumber, haveSymbol)
+    setPassword(pass)
+  }
+
   return (
     <div>
       <Head>
@@ -29,9 +38,14 @@ const Home: NextPage = () => {
       <main className="px-4 md:px-0 mx-auto max-w-[1080px]">        
         <div className="py-12 md:py-24">
           <div className="flex flex-col text-center w-full mb-5 md:mb-10">
-            <h1 className="text-2xl font-medium title-font mb-4 text-gray-900 dark:text-white">
-              {password}
-            </h1>
+            <div className="w-full max-w-sm mx-auto">
+              <div className="flex items-center border-b border-[#9CC3D5] py-2">
+                <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" value={password} onChange={handleChange} aria-label="Password"/>
+                <button className="flex-shrink-0 border-transparent border-4 text-[#9CC3D5] hover:text-[#9CC3D5cc] text-sm py-1 px-2 rounded" type="button" onClick={reloadNewPassword}>
+                  <FiRefreshCcw size={20} />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </main>
