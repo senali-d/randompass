67f918a15e4fc6852243b42d674c703599d0a8d6
Senali <senalidilumika@gmail.com>
2022-09-05 21:17:30 +0530

    Copy password to clipboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import { FiRefreshCcw } from 'react-icons/fi'
-
+import { MdContentCopy } from 'react-icons/md'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { generatePassword } from '../utils/util'
 
 const Home: NextPage = () => {
@@ -12,6 +13,8 @@ const Home: NextPage = () => {
   const [haveLowercase, setHaveLowercase] = useState<boolean>(false)
   const [haveNumber, setHaveNumber] = useState<boolean>(true)
   const [haveSymbol, setHaveSymbol] = useState<boolean>(true)
+  const [isCopied, setIsCopied] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
   
   useEffect(() => {
     const pass = generatePassword(size, haveUppercase, haveLowercase, haveNumber, haveSymbol)
@@ -23,8 +26,19 @@ const Home: NextPage = () => {
   }
 
   const reloadNewPassword = () => {
+    setIsClicked(true)
     const pass = generatePassword(size, haveUppercase, haveLowercase, haveNumber, haveSymbol)
     setPassword(pass)
+    setTimeout(() => {
+      setIsClicked(false)
+    }, 1000);
+  }
+
+  const copyPassword = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
   }
 
   return (
@@ -41,9 +55,22 @@ const Home: NextPage = () => {
             <div className="w-full max-w-sm mx-auto">
               <div className="flex items-center border-b border-[#9CC3D5] py-2">
                 <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" value={password} onChange={handleChange} aria-label="Password"/>
-                <button className="flex-shrink-0 border-transparent border-4 text-[#9CC3D5] hover:text-[#9CC3D5cc] text-sm py-1 px-2 rounded" type="button" onClick={reloadNewPassword}>
+                <button 
+                  className={`flex-shrink-0 border-transparent border-4 text-[#0063B2] hover:text-[#0063B2cc] text-sm py-1 px-2 rounded ${isClicked && 'rotate-180 transition-transform'}`}
+                  type="button" 
+                  onClick={reloadNewPassword}
+                >
                   <FiRefreshCcw size={20} />
                 </button>
+                <CopyToClipboard text={password} onCopy={copyPassword}>
+                  <button 
+                    className={`flex-shrink-0 border-transparent border-4 text-sm py-1 px-2 rounded ${isCopied ? 'text-[#9CC3D5]' : 'text-[#0063B2] hover:text-[#0063B2cc]'}`} 
+                    type="button" 
+                    disabled={isCopied ? true : false}
+                  >
+                    <MdContentCopy size={20} />
+                  </button>
+                </CopyToClipboard>
               </div>
             </div>
           </div>
